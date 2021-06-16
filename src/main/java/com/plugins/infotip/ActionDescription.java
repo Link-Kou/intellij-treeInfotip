71e308b35c8ee2c30ed5ad4eaff73612b23e828e
lk <502085708@qq.com>
2021-06-16 09:48:57 +0800

    fix: 修复路径替换方案

--- src/main/java/com/plugins/infotip/ActionDescription.java
+++ src/main/java/com/plugins/infotip/ActionDescription.java
@@ -26,42 +26,48 @@ public class ActionDescription extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        Project project = anActionEvent.getProject();
+        final Project project = anActionEvent.getProject();
         if (null == project) {
             return;
         }
-        XmlFile fileDirectoryXml = FileDirectory.getFileDirectoryXml(project, true);
-        List<XmlEntity> refreshXml = XmlParsing.getRefreshXml(project, fileDirectoryXml);
         //获取文件、文件夹等对象
         VirtualFile file = VIRTUAL_FILE.getData(anActionEvent.getDataContext());
         if (null != file) {
+            XmlFile fileDirectoryXml = FileDirectory.getFileDirectoryXml(project, true);
+            List<XmlEntity> refreshXml = XmlParsing.getRefreshXml(project, fileDirectoryXml);
             //使用相对路径
             String basePath = project.getPresentableUrl();
-            if (null == basePath) {
-                return;
-            }
-            String presentableUrl = file.getPresentableUrl();
-            String asbbasePath = presentableUrl.replace(basePath, "");
-            String extension = file.getExtension();
-            boolean notfind = false;
-            String txt = null;
-            for (XmlEntity x : refreshXml) {
-                if (presentableUrl.equals(x.getPath())) {
-                    txt = Messages.showInputDialog(project, "Input Your " + asbbasePath + "  Description",
-                            "What Needs To Be Description?", AllIcons.Actions.Menu_paste, x.getTitle(), null);
-                    notfind = true;
-                    if (null != txt) {
-                        XmlParsing.modifyPath(x.getTag(), txt, project);
+            if (null != basePath && basePath.length() > 0) {
+                //改为安长度去除
+                String presentableUrl = file.getPresentableUrl();
+                if (presentableUrl.length() < basePath.length()) {
+                    Messages.showMessageDialog(project, "Unable to get the root path of the file", "Can't Get Path", AllIcons.Actions.Menu_paste);
+                    return;
+                }
+                String asbbasePath = presentableUrl.substring(basePath.length(), presentableUrl.length());
+                String extension = file.getExtension();
+                boolean notfind = false;
+                String txt = null;
+                for (XmlEntity x : refreshXml) {
+                    if (presentableUrl.equals(x.getPath())) {
+                        txt = Messages.showInputDialog(project, "Input Your " + asbbasePath + "  Description",
+                                "What Needs To Be Description?", AllIcons.Actions.Menu_paste, x.getTitle(), null);
+                        notfind = true;
+                        if (null != txt) {
+                            XmlParsing.modifyPath(x.getTag(), txt, project);
+                        }
+                        break;
                     }
-                    break;
                 }
-            }
-            if (!notfind) {
-                txt = Messages.showInputDialog(project, "Input Your " + asbbasePath + "  Description",
-                        "What Needs To Be Description?", AllIcons.Actions.Menu_paste, "", null);
-                if (null != txt) {
-                    XmlParsing.createPath(fileDirectoryXml, project, asbbasePath, txt, extension);
+                if (!notfind) {
+                    txt = Messages.showInputDialog(project, "Input Your " + asbbasePath + "  Description",
+                            "What Needs To Be Description?", AllIcons.Actions.Menu_paste, "", null);
+                    if (null != txt) {
+                        XmlParsing.createPath(fileDirectoryXml, project, asbbasePath, txt, extension);
+                    }
                 }
+            } else {
+                Messages.showMessageDialog(project, "Unable to get the root path of the project", "Can't Get Path", AllIcons.Actions.Menu_paste);
             }
         }
     }
